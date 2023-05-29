3ccc8aa81e1f4414ddcaa5e14059125364c81f76
Mario Cerda <cerdamario13@gmail.com>
2023-05-29 15:35:00 -0500

    returning all runs function

--- src/activities.py
+++ src/activities.py
@@ -1,24 +1,29 @@
 import wrangles
 import pandas as pd
-import datetime
+import json
 
-# Inputs for running activities
-data_file = "Garmin_Data.csv"
-data_path = f"data/{data_file}"
-recipe_file = "src/activities.wrgl.yaml"
+def all_runs():
+    """
+    Return all runs from the data file
+    """
+        # Inputs for running activities
+    data_file = "Garmin_Data.csv"
+    data_path = f"data/{data_file}"
+    recipe_file = "src/activities.wrgl.yaml"
 
-# Select Activities Type Column that contains the a keyword (run)
-query = """
-SELECT *
-FROM df
-WHERE Activity_Type LIKE '%run%'
-"""
+    # Select Activities Type Column that contains the a keyword (run)
+    query = """
+    SELECT *
+    FROM df
+    WHERE Activity_Type LIKE '%run%'
+    """
 
-vars = {
-    "data_path": data_path,
-    "query": query,
-}
+    vars = {
+        "data_path": data_path,
+        "query": query,
+    }
 
-df = wrangles.recipe.run(recipe=recipe_file, variables=vars)
-print(df)
+    df = wrangles.recipe.run(recipe=recipe_file, variables=vars)
+    df = df.to_json(orient="table")
 
+    return json.loads(df)
