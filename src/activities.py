08a9a70940960ba781647260f615c1e33e0ed3ea
Mario Cerda <cerdamario13@gmail.com>
2023-05-29 20:36:12 -0500

    get summaries function

--- src/activities.py
+++ src/activities.py
@@ -28,41 +28,54 @@ def all_runs():
     df = df.to_json(orient="table")
 
     return json.loads(df)
+    
 
-"""
-Return a summary of activities
-"""
+def all_runs_summaries():
+    """
+    Return a summary of activities
+    """
 
-# Inputs for running activities
-data_file = "Garmin_Data.csv"
-data_path = f"data/{data_file}"
-recipe_file = "src/activities.wrgl.yaml"
-
-
-query = """
-SELECt *
-FROM df
-WHERE Activity_Type LIKE '%run%'
-"""
-
-vars = {
-    "data_path": data_path,
-    "query": query
-}
-
-df = wrangles.recipe.run(recipe=recipe_file, variables=vars)
-
-# Overall Sums
-total_distance_traveled = df['Distance'].astype('float').sum()
-total_calories = df['Calories'].astype('float').sum()
-
-# Averages
-average_distance_per_activity = df['Distance'].astype('float').mean().round(2)
-average_calories_per_activity = df['Calories'].astype('float').mean().round(2)
-average_heart_rate_per_activity = df['Avg HR'].astype('float').mean().round(2)
-
-tmp_time = df['TimeNew'].apply(lambda x: datetime.datetime.strptime(x, '%H:%M:%S').time())
+    # Inputs for running activities
+    data_file = "Garmin_Data.csv"
+    data_path = f"data/{data_file}"
+    recipe_file = "src/activities.wrgl.yaml"
 
 
+    query = """
+    SELECt *
+    FROM df
+    WHERE Activity_Type LIKE '%run%'
+    """
 
-print(df)
+    vars = {
+        "data_path": data_path,
+        "query": query
+    }
+
+    df = wrangles.recipe.run(recipe=recipe_file, variables=vars)
+
+    # Overall Sums
+    total_distance = df['Distance'].astype('float').sum()
+    total_calories = df['Calories'].astype('float').sum()
+    total_time = {
+        'days': pd.to_timedelta(df['Time']).sum().components.days,
+        'hours': pd.to_timedelta(df['Time']).sum().components.hours,
+        'minutes': pd.to_timedelta(df['Time']).sum().components.minutes
+    }
+
+    # Averages
+    average_distance_per_activity = df['Distance'].astype('float').mean().round(2)
+    average_calories_per_activity = df['Calories'].astype('float').mean().round(2)
+    average_heart_rate_per_activity = df['Avg HR'].astype('float').mean().round(2)
+
+    summaries_dict = {
+        'total_distance': total_distance,
+        'total_calories': total_calories,
+        'total_time': total_time,
+        'average_distance_per_activity': average_distance_per_activity,
+        'average_calories_per_activity': average_calories_per_activity,
+        'average_heart_rate_per_activity': average_heart_rate_per_activity
+    }
+
+    return summaries_dict
+
--- src/app.py
+++ src/app.py
@@ -16,6 +16,21 @@ def get_all_runs():
 
     return jsonify(results)
 
+@app.route('/runSummaries', methods=['POST'])
+def get_run_summaries():
+    """
+    Get summaries for all runs
+
+    total_distance
+    total_calories
+    total_time
+    average_distance_per_activity
+    average_calories_per_activity
+    average_heart_rate_per_activity
+    """
+
+    results = activities.all_runs_summaries()
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=False)
